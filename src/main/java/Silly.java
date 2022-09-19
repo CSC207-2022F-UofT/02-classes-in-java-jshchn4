6092a529625e6ccf00c8d61d6d8bc923c13dbf8a
jshchn4 <joshtyler.chan@mail.utoronto.ca>
2022-09-19 18:10:24 -0400

    Attempt 2

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -115,11 +115,11 @@ public class Silly implements Comparable<Silly>{
          *                Fill out the expected_values array with the
          *                values that countStatic will return at each call.
          */
-        x.countStatic();
-        y.countStatic();
-        x.countStatic();
-        x.countStatic();
-        int[] expected_values = {1, 2, 3, 4};
+        int a = x.countStatic();
+        int b = y.countStatic();
+        int c = x.countStatic();
+        int d = x.countStatic();
+        int[] expected_values = {a, b, c, d};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
